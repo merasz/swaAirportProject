37a306bcadcb3d5de70313a1fe49781ba1a61af7
raffael <raffael.prem@student.uibk.ac.at>
2019-12-11 22:26:33 +0100

    aircraftCreation_v0.1

---
+++ src/main/java/at/qe/sepm/skeleton/ui/controllers/AircraftDetailController.java
@@ -0,0 +1,71 @@
+package at.qe.sepm.skeleton.ui.controllers;
+
+import at.qe.sepm.skeleton.model.Aircraft;
+import at.qe.sepm.skeleton.services.AircraftService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Controller for the aircraft detail view
+ **/
+
+@Component
+@Scope("view")
+public class AircraftDetailController {
+
+    @Autowired
+    private AircraftService aircraftService;
+
+    /**
+     * Attribute to cache the currently displayed aircraft
+     */
+    private Aircraft aircraft;
+
+    /**
+     * Sets the currently displayed aircraft and reloads it form db. This aircraft is
+     * targeted by any further calls of
+     * {@link #doReloadAircraft()}, {@link #doSaveAircraft()} and
+     * {@link #doDeleteAircraft()}.
+     *
+     * @param aircraft
+     */
+    public void setAircraft(Aircraft aircraft) {
+        this.aircraft = aircraft;
+        doReloadAircraft();
+    }
+
+    /**
+     * Returns the currently displayed aircraft.
+     *
+     * @return
+     */
+    public Aircraft getAircraft() {
+        return aircraft;
+    }
+
+    /**
+     * Action to force a reload of the currently displayed aircraft.
+     */
+    public void doReloadAircraft() {
+        aircraft = aircraftService.loadAircraft(aircraft.getAircraftId());
+    }
+
+    /**
+     * Action to save the currently displayed aircraft.
+     */
+    public void doSaveAircraft() {
+
+        aircraft = this.aircraftService.saveAircraft(aircraft);
+    }
+
+    /**
+     * Action to delete the currently displayed aircraft.
+     */
+    public void doDeleteAircraft() {
+        this.aircraftService.deleteAircraft(aircraft);
+        aircraft = null;
+    }
+
+}
