6edc992cf8f0ca9526a7235a8daa6888768456b2
raffael <raffael.prem@student.uibk.ac.at>
2019-12-11 22:27:02 +0100

    aircraftCreation_v0.1

--- src/main/java/at/qe/sepm/skeleton/repositories/AircraftRepository.java
+++ src/main/java/at/qe/sepm/skeleton/repositories/AircraftRepository.java
@@ -1,5 +1,20 @@
 package at.qe.sepm.skeleton.repositories;
 
-public interface AircraftRepository {
+import at.qe.sepm.skeleton.model.Aircraft;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+/**
+ * Repository for managing Aircraft entities
+ */
+
+public interface AircraftRepository extends AbstractRepository<Aircraft, String>{
+
+    Aircraft findFirstByAircraftId(String aircraftId);
+
+    List<Aircraft> findByAircraftIdContaining(String aircraftId);
+
+    @Query("SELECT u FROM Aircraft u WHERE u.aircraftId = :aircraftId")
+    List<Aircraft> findByAircraftId(@Param("aircraftId") String aircraftId);
 }
