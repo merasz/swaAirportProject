6d744cc70b0ee165ba1e000af508ecbdbf69e2cc
raffael <raffael.prem@student.uibk.ac.at>
2019-12-11 22:27:13 +0100

    aircraftCreation_v0.1

--- src/main/java/at/qe/sepm/skeleton/model/Aircraft.java
+++ src/main/java/at/qe/sepm/skeleton/model/Aircraft.java
@@ -1,24 +1,58 @@
 package at.qe.sepm.skeleton.model;
 
-public class Aircraft {
-	private int aircraftId;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Set;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.data.domain.Persistable;
+
+
+/**
+ *
+ * Entity representing an aircraft
+ */
+
+@Entity
+public class Aircraft implements Persistable<String>, Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(length =100)
+	private String aircraftId;
+
+	@ManyToOne(optional = false)
+	private Aircraft createAircraft;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createDate;
+	@ManyToOne(optional = true)
+	private Aircraft updateAircraft;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date updateDate;
+
 	private String aircraftType;
 	private int requiredPilotsAircraft;
 	private int requiredBoardpersonalAircraft;
 	private int capacityAircraft;
 	
-	public Aircraft(String aircraftType, int requiredPilotsAircraft, int requiredBoardpersonalAircraft, int capacityAircraft) {
-		this.aircraftType = aircraftType;
-		this.requiredPilotsAircraft = requiredPilotsAircraft;
-		this.requiredBoardpersonalAircraft = requiredBoardpersonalAircraft;
-		this.capacityAircraft = capacityAircraft;
-	}
 
-	public int getAircraftId() {
+
+	public String getAircraftId() {
 		return aircraftId;
 	}
 
-	public void setAircraftId(int aircraftId) {
+	public void setAircraftId(String aircraftId) {
 		this.aircraftId = aircraftId;
 	}
 
@@ -53,8 +87,75 @@ public class Aircraft {
 	public void setCapacityAircraft(int capacityAircraft) {
 		this.capacityAircraft = capacityAircraft;
 	}
-	
-	
-	
-	
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Aircraft getUpdateAircraft() {
+		return updateAircraft;
+	}
+
+	public void setUpdateAircraft(Aircraft updateAircraft) {
+		this.updateAircraft = updateAircraft;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public Aircraft getCreateAircraft() {
+		return createAircraft;
+	}
+
+	public void setCreateAircraft(Aircraft createAircraft) {
+		this.createAircraft = createAircraft;
+	}
+
+	@Override
+	public String toString() {
+		return "at.qe.sepm.skeleton.model.Aircraft[ id=" + aircraftId + " ]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Aircraft)) {
+			return false;
+		}
+		final Aircraft other = (Aircraft) obj;
+		if (!Objects.equals(this.aircraftId, other.aircraftId)) {
+			return false;
+		}
+		return true;
+	}
+
+	public int hashCode() {
+		return java.util.Objects.hash(super.hashCode(), aircraftId, aircraftType, requiredPilotsAircraft, requiredBoardpersonalAircraft, capacityAircraft);
+	}
+
+
+	@Override
+	public String getId() {
+		return getAircraftId();
+	}
+
+	public void setId(String id) {
+		setAircraftId(id);
+	}
+
+	@Override
+	public boolean isNew() {
+		return (null == createDate);
+	}
 }
