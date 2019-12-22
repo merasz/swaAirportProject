b6e61fbd228091b5fdc7aadc4f45d7cf3147cc8f
raffael <raffael.prem@student.uibk.ac.at>
2019-12-22 17:51:52 +0100

    userCreation fixed flight creation holiday creation aircraft creation all services / repo's / controllers list + detail

--- src/main/java/at/qe/sepm/skeleton/repositories/AircraftRepository.java
+++ src/main/java/at/qe/sepm/skeleton/repositories/AircraftRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+
 /**
  * Repository for managing Aircraft entities
  */
