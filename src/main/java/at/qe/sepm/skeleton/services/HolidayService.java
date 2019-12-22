ccf08db43dd3af984bb3a773461fa05973f1de00
raffael <raffael.prem@student.uibk.ac.at>
2019-12-22 17:52:37 +0100

    userCreation fixed flight creation holiday creation aircraft creation all services / repo's / controllers list + detail

--- src/main/java/at/qe/sepm/skeleton/services/HolidayService.java
+++ src/main/java/at/qe/sepm/skeleton/services/HolidayService.java
@@ -1,9 +1,8 @@
 package at.qe.sepm.skeleton.services;
 
-import at.qe.sepm.skeleton.model.User;
+
 import at.qe.sepm.skeleton.model.Holiday;
 import at.qe.sepm.skeleton.repositories.HolidayRepository;
-import at.qe.sepm.skeleton.repositories.UserRepository;
 import java.util.Collection;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
 
 /**
  * Service for accessing and manipulating holiday data.
@@ -27,9 +27,8 @@ public class HolidayService {
     @Autowired
     private HolidayRepository holidayRepository;
 
-    private UserService userService;
 
-    private UserRepository userRepository;
+
 
     /**
      * Returns a collection of all holidays.
@@ -42,9 +41,6 @@ public class HolidayService {
     }
 
     public Collection <Holiday> getHolidayByUser(String username){return holidayRepository.findByUsername(username);}
-
-
-    public Collection<Holiday> testGet(){return holidayRepository.test();}
 
 
     /**
@@ -71,7 +67,7 @@ public class HolidayService {
      * @return the updated holiday
      */
 
-    public Holiday saveHoliday(Holiday holiday) {
+    public Holiday saveHoliday(Holiday holiday){
         if (holiday.isNew()) {
             holiday.setCreateDate(new Date());
 
@@ -81,7 +77,6 @@ public class HolidayService {
         }
 
 
-        holiday.setUsername(getAuthenticatedUsername());
         return holidayRepository.save(holiday);
     }
 
@@ -91,16 +86,9 @@ public class HolidayService {
      *
      * @param holiday the holiday to delete
      */
-
-    public void deleteHoliday(Holiday holiday) {
-        holidayRepository.delete(holiday);
+    public void deleteHoliday(Long id) {
+        holidayRepository.deleteById(id);
         // :TODO: write some audit log stating who and when this holiday was permanently deleted.
     }
-
-    private String getAuthenticatedUsername() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        return userRepository.findFirstByUsername(auth.getName()).getUsername();
-    }
-
 
 }
