da9be109b44eb6e12599dd5c1c9149cdfdb791c8
Elias Gendu <elias.gendu@students.ibk.ac.at>
2020-01-05 21:51:49 +0100

    Email-Service

---
+++ src/main/java/at/qe/sepm/skeleton/services/EmailService.java
@@ -0,0 +1,48 @@
+package at.qe.sepm.skeleton.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+public class EmailService{
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    public void sendMessage(String to, String subject, String text){
+        if(emailSender==null){
+            emailSender = getEmailSender();
+        }
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+
+    @Bean
+    public JavaMailSender getEmailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("softwarearch20@gmail.com");
+        mailSender.setPassword("software123MAIL");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+
+}
