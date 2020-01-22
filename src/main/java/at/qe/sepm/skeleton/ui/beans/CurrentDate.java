15721d3ee5402ea3f337db2c833b9b194b6eae9e
Michael Andre Hauser <michael.a.hauser@student.uibk.ac.at>
2020-01-22 09:07:24 +0100

    some validation was added for flight html

---
+++ src/main/java/at/qe/sepm/skeleton/ui/beans/CurrentDate.java
@@ -0,0 +1,24 @@
+package at.qe.sepm.skeleton.ui.beans;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("request")
+public class CurrentDate {
+	public Date getCurrentDate() {
+		return (new Date());
+	}
+	
+	public Date getCurrentDateArr() {
+	    Calendar calendar = Calendar.getInstance();
+	    Date date = new Date();
+		calendar.setTime(date);
+	    calendar.add(Calendar.HOUR_OF_DAY, 1);
+	    return calendar.getTime();
+	}
+	
+}
